1620a199c3169eba049c116b7b5be0c6a80ef95f
mujib shaikh <mujibshaikh3231@gmail.com>
2022-07-14 15:42:22 +0530

    Updates

---
+++ precache-manifest.2ee2221ffbfbf78dff9f0887920c9ebe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3338ee29c8fa9224504",
+    "url": "/Todolistv2/static/js/main.c3338ee2.chunk.js"
+  },
+  {
+    "revision": "119c0e91d039c52e00e8",
+    "url": "/Todolistv2/static/js/1.119c0e91.chunk.js"
+  },
+  {
+    "revision": "32ab6b20a8f020f4672e",
+    "url": "/Todolistv2/static/js/runtime~main.32ab6b20.js"
+  },
+  {
+    "revision": "d44ea21d3c198ab1edc8c5ddb75b8861",
+    "url": "/Todolistv2/index.html"
+  }
+];
